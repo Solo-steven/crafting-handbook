f2b5a642b831850456cdc53f8729e2431800407f
Solo-steven <ssteven19@kimo.com>
2024-01-05 22:47:37 +0800

    feat: setup basic frontend scene

---
+++ website/frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+import { Navbar } from "@/src/Navbar";
+export default function Home() {
+  return (
+    <div className=" bg-slate-900 stroke-slate-900 w-screen h-screen">
+       <Navbar />
+    </div>
+  )
+}
---
+++ website/frontend/src/Navbar/Navbar.tsx
@@ -0,0 +1,8 @@
+import { BiLogoGithub } from "react-icons/bi";
+export const Navbar = () => {
+    return (
+        <div >
+            <BiLogoGithub />
+        </div>
+    );
+}
